1a9eba17777a8c684bf32302f2e7c82e0c06be95
Daniel <Danielbornstein98@gmail.com>
2016-06-29 13:25:02 -0400

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friends = [
+	{
+		"name": "Jane",
+		"photo": "http://dreamatico.com/data_images/girl/girl-8.jpg",
+		"scores": [3, 2, 5, 1, 4, 2, 2, 4, 3, 1]
+	},
+	{
+		"name": "Margaret",
+		"photo": "https://www.girldevelopit.com/assets/board/vanessahurst-117e059b377bcd9649f0f7fd77a10d03.jpg",
+		"scores": [3, 2, 1, 5, 3, 4, 2, 1, 4, 3]
+	}
+];
+
+module.exports = friends;
